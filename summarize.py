ebd2735c6a66606b217986a478f4d94d2074286e
sivaprakashrud <sivaprakashsvofficial@gmail.com>
2025-06-03 13:49:35 +0530

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
-def summarize_text(text, max_length=300, min_length=100):
+def summarize_text(text, max_length=500, min_length=100):
     try:
         summary = summarizer(text, max_length=max_length, min_length=min_length, do_sample=False)
         return summary[0]['summary_text']
