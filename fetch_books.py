7e9b7565d5e6a6d41e887761209196058450b44a
sivaprakashrud <sivaprakashsvofficial@gmail.com>
2025-06-07 13:51:24 +0530

    Update fetch_books.py

--- fetch_books.py
+++ fetch_books.py
@@ -44,7 +44,7 @@ def fetch_from_google_books():
     except Exception as e:
         print(f"[ERROR] Google Books fetch failed: {e}")
     return books
-
+os.makedirs("data", exist_ok=True)
 def save_books_to_file(book_list, filename="data/books.json"):
     try:
         with open(filename, "w", encoding="utf-8") as f:
