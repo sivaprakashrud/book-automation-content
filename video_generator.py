270929f04adfc5a877426826e76314bc24419ba8
sivaprakashrud <sivaprakashsvofficial@gmail.com>
2025-06-12 08:50:18 +0530

    Update video_generator.py

--- video_generator.py
+++ video_generator.py
@@ -46,12 +46,12 @@ def generate_videos(summary_file=SUMMARY_FILE, voice_dir=VOICE_DIR, output_dir=V
             # Text overlay
             txt_clip = TextClip(
                 summary,
-                fontsize=(40,"DejaVu-Sans"),
-                color='white',
+                method='caption',
                 size=(1000, 1700),
-                method='caption'
-            )
-
+                fontsize=40,
+                font="DejaVu-Sans",
+                color='white'
+            ).set_duration(duration).set_position("center")
 
             # Final video
             video = CompositeVideoClip([background, txt_clip]).set_audio(audioclip)
